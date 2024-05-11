ddcec620c6008bf077dda103e9f8db37aa98fd9d
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-05-11 15:30:04 +0700

    add basic static resources (page & assets)

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"net/http"
+	//"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.File("/", "resources/pages/index.html")
+	e.Static("/assets", "resources/assets")
 
 	e.GET("/.well-known/openid-configuration", OpenidconfigHandler)
 
