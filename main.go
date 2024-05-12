240a97485bddee0f152b3d018db79ab479474235
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-05-13 06:24:56 +0700

    Feat/embed (#1)
    * feat: attempt to embed resources
    
    * finalize embeded file handler

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+//go:embed resources
+var embededFiles embed.FS
+
 func main() {
   APP_PORT := GetEnvOrDefault("APP_PORT", "3000")
   GenerateSigningKeys()
-
 	e := echo.New()
-	e.File("/", "resources/pages/index.html")
-	e.Static("/assets", "resources/assets")
+
+  assetHandler := http.FileServer(getFileSystem())
+	e.GET("/", serveEmbededFile("resources/pages/index.html"))
+	e.GET("/assets/*", echo.WrapHandler(assetHandler))
 
 	e.GET("/.well-known/certs", func (c echo.Context) error {
 		return c.JSON(http.StatusOK, PublicJWKS)
@@ -21,4 +27,19 @@ func main() {
   GeneralOAuthModuleInit(e)
 
 	e.Logger.Fatal(e.Start(":" + APP_PORT))
+}
+
+func serveEmbededFile(path string) echo.HandlerFunc {
+  return echo.WrapHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    http.ServeFileFS(w, r, embededFiles, path)
+  }))
+}
+
+func getFileSystem() http.FileSystem {
+	fsys, err := fs.Sub(embededFiles, "resources")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
 }
