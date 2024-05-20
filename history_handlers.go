c2dcbf12739ad4015e458d24d4e70fd7b86ca96e
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-05-20 21:48:58 +0700

    split history_handlers & template_renderer from main.go

---
+++ history_handlers.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+  "log"
+  "strings"
+	"net/http"
+  "encoding/json"
+  "encoding/base64"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterHistoryHandlers(app *echo.Echo) {
+  app.Use(extractBasicAuthMiddleware)
+  app.Use(historyRecorderMiddleware)
+
+	app.GET("/manage/history", ServeResourceFile("resources/pages/history_detail.html"))
+  app.GET("/manage/history__", historyDetailHandler)
+
+}
+
+func historyDetailHandler(c echo.Context) error {
+  clientId := c.QueryParam("id")
+  from := c.QueryParam("from")
+  histories, err := HistoryRepository.All(clientId, from)
+  if err != nil {
+    return err
+  }
+
+  templ := `
+      <div id="history-list">
+        <form hx-get="/manage/history__" hx-target="#history-list">
+          <label for="">client_id</label>
+          <input type="text" name="id" value="{{ .client_id }}">
+          <input type="hidden" name="from" value="0">
+          <button type="submit">get</button>
+        </form>
+
+        <table>
+          <thead>
+            <tr>
+              <th>timestamp</th>
+              <th>data</th>
+            </tr>
+          </thead>
+          <tbody>
+          {{ if len .histories | ge 0 }}
+              empty data for client "{{ .client_id }}"
+            {{ else }}
+              {{ range .histories }}
+              <tr>
+                <td>{{ .Timestamp }}</td>
+                <td>
+                <textarea style="width:100%" rows="5" disabled>{{ .Data }}</textarea>
+                </td>
+              </tr>
+              {{ end }}
+            {{ end }}
+          </tbody>
+        </table>
+      </div>
+  `
+  res, err := RenderHTML(templ, map[string]interface{}{
+    "histories": histories,
+    "client_id": clientId,
+  })
+  if err != nil {
+    log.Printf("found err %v\n", err)
+    return err
+  }
+
+  return c.HTML(http.StatusOK, res)
+}
+
+func extractBasicAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+  return func(c echo.Context) error {
+    // Get the Authorization header
+    authHeader := c.Request().Header.Get("Authorization")
+    if authHeader == "" {
+      return next(c)
+    }
+
+    // Check if the Authorization header starts with "Basic "
+    if !strings.HasPrefix(authHeader, "Basic ") {
+      return next(c)
+    }
+
+    // Decode the base64 part of the header
+    base64Credentials := strings.TrimPrefix(authHeader, "Basic ")
+    credentials, err := base64.StdEncoding.DecodeString(base64Credentials)
+    if err != nil {
+      return next(c)
+    }
+
+    // Split the credentials into username and password
+    creds := strings.SplitN(string(credentials), ":", 2)
+    if len(creds) != 2 {
+      return next(c)
+    }
+
+    c.Set("basic-auth-username", creds[0])
+    c.Set("basic-auth-password", creds[1])
+
+    return next(c)
+  }
+}
+
+
+
+func historyRecorderMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+  return func(c echo.Context) error {
+    var clientId string
+
+    if v := c.Get("basic-auth-username"); v != nil {
+      clientId = v.(string)
+    }
+    if v := c.QueryParam("client_id"); v != "" {
+      clientId = v
+    }
+    if v := c.FormValue("client_id"); v != "" {
+      clientId = v
+    }
+
+    if (clientId != "") {
+      log.Printf("got client_id=%v\n", clientId)
+
+      // Get request headers
+      headers := make(map[string]string)
+      for name, values := range c.Request().Header {
+        // Combine multiple values for the same header into a single string
+        headers[name] = values[0]
+      }
+
+      formParams, _ := c.FormParams()
+
+      data := map[string]interface{}{
+        "http_method": c.Request().Method,
+        "url": c.Request().URL.String(),
+        "headers": headers,
+        "form_params": formParams,
+        "query_params": c.QueryParams(),
+      }
+      bytes, _ := json.Marshal(data)
+      HistoryRepository.Record(clientId, string(bytes))
+    }
+
+    return next(c)
+  }
+}
+
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-  "io"
   "log"
-  "strings"
 	"net/http"
-  "html/template"
-  "encoding/json"
-  "encoding/base64"
 
 	"github.com/labstack/echo/v4"
 )
@@ -22,8 +17,8 @@ func main() {
 
 	e := echo.New()
 
-  e.Use(extractBasicAuthMiddleware)
-  e.Use(historyRecorderMiddleware)
+  e.Renderer = NewTemplateRenderer()
+  RegisterHistoryHandlers(e)
 
 	e.GET("/", ServeResourceFile("resources/pages/index.html"))
 	e.GET("/assets/*", ServeResourceFolder("resources"))
@@ -32,179 +27,9 @@ func main() {
 		return c.JSON(http.StatusOK, PublicJWKS)
   })
 
-  e.Renderer = NewTemplateRenderer()
-	e.GET("/manage/history", ServeResourceFile("resources/pages/history_detail.html"))
-  e.GET("/manage/history__", historyDetailHandler)
-
   RegisterGeneralOAuthModule(e)
 
 	e.Logger.Fatal(e.Start(":" + APP_PORT))
 }
 
-type Template struct {
-  templates *template.Template
-}
-
-// Render method to render the template string
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-  tmpl, err := template.New("htmlTemplate").Parse(name)
-  if err != nil {
-    return err
-  }
-  return tmpl.Execute(w, data)
-}
-
-func NewTemplateRenderer() *Template {
-  renderer := &Template{
-    templates: template.Must(template.New("T").Parse("")),
-  }
-
-  return renderer
-}
-
-func renderHTML(htmlTemplate string, data interface{}) (string, error) {
-  // Parse and execute the template
-  tmpl, err := template.New("htmlTemplate").Parse(htmlTemplate)
-  if err != nil {
-    return "", err
-  }
-
-  // Create a buffer to hold the executed template
-  var renderedContent strings.Builder
-  if err := tmpl.Execute(&renderedContent, data); err != nil {
-    return "", err
-  }
-
-  // Return the rendered HTML
-  return renderedContent.String(), nil
-}
-
-func historyDetailHandler(c echo.Context) error {
-  clientId := c.QueryParam("id")
-  from := c.QueryParam("from")
-  histories, err := HistoryRepository.All(clientId, from)
-  if err != nil {
-    return err
-  }
-
-  templ := `
-      <div id="history-list">
-        <form hx-get="/manage/history__" hx-target="#history-list">
-          <label for="">client_id</label>
-          <input type="text" name="id" value="{{ .client_id }}">
-          <input type="hidden" name="from" value="0">
-          <button type="submit">get</button>
-        </form>
-
-        <table>
-          <thead>
-            <tr>
-              <th>timestamp</th>
-              <th>data</th>
-            </tr>
-          </thead>
-          <tbody>
-          {{ if len .histories | ge 0 }}
-              empty data for client "{{ .client_id }}"
-            {{ else }}
-              {{ range .histories }}
-              <tr>
-                <td>{{ .Timestamp }}</td>
-                <td>
-                <textarea style="width:100%" rows="5" disabled>{{ .Data }}</textarea>
-                </td>
-              </tr>
-              {{ end }}
-            {{ end }}
-          </tbody>
-        </table>
-      </div>
-  `
-  res, err := renderHTML(templ, map[string]interface{}{
-    "histories": histories,
-    "client_id": clientId,
-  })
-  if err != nil {
-    log.Printf("found err %v\n", err)
-    return err
-  }
-
-  return c.HTML(http.StatusOK, res)
-}
-
-func extractBasicAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-  return func(c echo.Context) error {
-    // Get the Authorization header
-    authHeader := c.Request().Header.Get("Authorization")
-    if authHeader == "" {
-      return next(c)
-    }
-
-    // Check if the Authorization header starts with "Basic "
-    if !strings.HasPrefix(authHeader, "Basic ") {
-      return next(c)
-    }
-
-    // Decode the base64 part of the header
-    base64Credentials := strings.TrimPrefix(authHeader, "Basic ")
-    credentials, err := base64.StdEncoding.DecodeString(base64Credentials)
-    if err != nil {
-      return next(c)
-    }
-
-    // Split the credentials into username and password
-    creds := strings.SplitN(string(credentials), ":", 2)
-    if len(creds) != 2 {
-      return next(c)
-    }
-
-    c.Set("basic-auth-username", creds[0])
-    c.Set("basic-auth-password", creds[1])
-
-    return next(c)
-  }
-}
-
-
-
-func historyRecorderMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-  return func(c echo.Context) error {
-    var clientId string
-
-    if v := c.Get("basic-auth-username"); v != nil {
-      clientId = v.(string)
-    }
-    if v := c.QueryParam("client_id"); v != "" {
-      clientId = v
-    }
-    if v := c.FormValue("client_id"); v != "" {
-      clientId = v
-    }
-
-    if (clientId != "") {
-      log.Printf("got client_id=%v\n", clientId)
-
-      // Get request headers
-      headers := make(map[string]string)
-      for name, values := range c.Request().Header {
-        // Combine multiple values for the same header into a single string
-        headers[name] = values[0]
-      }
-
-      formParams, _ := c.FormParams()
-
-      data := map[string]interface{}{
-        "http_method": c.Request().Method,
-        "url": c.Request().URL.String(),
-        "headers": headers,
-        "form_params": formParams,
-        "query_params": c.QueryParams(),
-      }
-      bytes, _ := json.Marshal(data)
-      HistoryRepository.Record(clientId, string(bytes))
-    }
-
-    return next(c)
-  }
-}
 
