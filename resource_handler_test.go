22b39e365e7f59a68fd30b6b4c8c03bbccde57ed
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-09-16 06:11:19 +0700

    progres 3

--- resource_handler_test.go
+++ resource_handler_test.go
@@ -8,17 +8,16 @@ import (
 	"testing"
 
 	"github.com/labstack/echo/v4"
-	//"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 type testCase struct {
   name               string
   requestUrl        string
-  //requestBody        string
   expectedStatusCode int
-  //expectedResponse   string
+  expectedResponseContains   []string
+  expectedResponseNotContains   []string
 }
-
 
 func TestServeResourceFolder(t *testing.T) {
 	// Setup
@@ -30,15 +29,74 @@ func TestServeResourceFolder(t *testing.T) {
 		{
 			name:               "Valid request",
 			requestUrl:        `/assets/tacit.min.css`,
-			//requestBody:        `/assets/tacit.min.css`,
 			expectedStatusCode: http.StatusOK,
-			//expectedResponse:   `{"id_token":"valid_id_token","access_token":"valid_access_token"}`,
+      expectedResponseContains: nil,
+      expectedResponseNotContains: nil,
+		},
+	}
+
+  runTests(tests, testHandler, e, t)
+}
+
+func TestServeResourceFile(t *testing.T) {
+	e := echo.New()
+
+  testHandler := ServeResourceFile("resources/views/home.html")
+
+	tests := []testCase {
+		{
+			name:               "Valid request",
+			requestUrl:        `/`,
+			expectedStatusCode: http.StatusOK,
+      expectedResponseContains: []string{
+        "<p>To start playing with this tools, you could use any OIDC client you have",
+        `<script src="/assets/htmx.min.js"></script>`, // script in layout_base.html
+      },
+      expectedResponseNotContains: []string{
+        "random non-existing string",
+      },
+		},
+	}
+
+  runTests(tests, testHandler, e, t)
+
+  testHandlerFail := ServeResourceFile("not-existing.html")
+
+	tests = []testCase {
+		{
+			name:               "Requesting non-existing template",
+			requestUrl:        `/`,
+			expectedStatusCode: http.StatusNotFound,
+      expectedResponseContains: nil,
+      expectedResponseNotContains: nil,
+		},
+	}
+
+  runTests(tests, testHandlerFail, e, t)
+}
+
+func TestServeResourceTemplate(t *testing.T) {
+	// Setup
+	e := echo.New()
+
+	e.Renderer = SetupTemplateRegistry("resources/views/*")
+  testHandler := ServeResourceTemplate("resources/views/home.html")
+	e.GET("/", testHandler)
+
+	tests := []testCase {
+		{
+			name:               "Valid request",
+			requestUrl:        `/`,
+			expectedStatusCode: http.StatusOK,
+      expectedResponseContains: nil,
+      expectedResponseNotContains: nil,
 		},
 		{
 			name:               "Not found",
-			requestUrl:        `/random-non-existing-file.min.css`,
+			requestUrl:        `/not-exists`,
 			expectedStatusCode: http.StatusNotFound,
-			//expectedResponse:   `{"id_token":"valid_id_token","access_token":"valid_access_token"}`,
+      expectedResponseContains: nil,
+      expectedResponseNotContains: nil,
 		},
 	}
 
@@ -49,23 +107,29 @@ func runTests(testcases []testCase, testHandler echo.HandlerFunc, e *echo.Echo, 
 	for _, tt := range testcases {
 		t.Run(tt.name, func(t *testing.T) {
 			req := httptest.NewRequest(http.MethodGet, tt.requestUrl, nil)
-			//req := httptest.NewRequest(http.MethodGet, tt.requestUrl, strings.NewReader(tt.requestBody))
-			//req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
 
 			err := testHandler(c)
 			if err != nil {
-				t.Errorf("tokenHandler() returned error: %v", err)
+				t.Errorf("handler returned error: %v", err)
 			}
 
 			if rec.Code != tt.expectedStatusCode {
 				t.Errorf("expected status %d, got %d", tt.expectedStatusCode, rec.Code)
 			}
 
-			//if strings.TrimSpace(rec.Body.String()) != tt.expectedResponse {
-				//t.Errorf("expected response %q, got %q", tt.expectedResponse, rec.Body.String())
-			//}
+      if tt.expectedResponseContains != nil {
+        for _, contains := range tt.expectedResponseContains {
+          assert.Contains(t, rec.Body.String(), contains)
+        }
+      }
+
+      if tt.expectedResponseNotContains != nil {
+        for _, contains := range tt.expectedResponseNotContains {
+          assert.NotContains(t, rec.Body.String(), contains)
+        }
+      }
 		})
 	}
 }
